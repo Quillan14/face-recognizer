24d088a041c59af0005a098bb02ef003f6babcdf
Quillan14 <128583075+Quillan14@users.noreply.github.com>
2023-12-13 18:59:22 +1100

    Add files via upload

---
+++ Image_recognizer-20201209T172621Z-001 (1)/Image_recognizer/main.js
@@ -0,0 +1,15 @@
+Webcam.set({
+    width:350
+    height:300
+    image_format : 'png'
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+function take_snapshot()
+{
+    webcam,snap(funtction)
+}
